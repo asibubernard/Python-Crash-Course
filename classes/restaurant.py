0414127510bba2a64825c4a7023d7a4e61550ac3
Bernard Asibu <44650300+asibubernard@users.noreply.github.com>
2022-09-14 23:38:22 -0400

    Modified Restaurant and Users modules

--- classes/restaurant.py
+++ classes/restaurant.py
@@ -1,23 +1,36 @@
 class Restaurant():
     """A simple restaurant model."""
 
-    def __init__(self, name, cuisine, location, star):
+    def __init__(self, name, cuisine, location, star,):
         """Initialize name, cuisine and location attributes."""
         self.name = name
         self.cuisine = cuisine
         self.location = location
         self.star = star
+        self.number_served = 0
 
     def describe_restaurant(self):
         """Description of a restaurant."""
         print(self.name.title() + " is the name a " + str(self.star) + " star restaurant.")
         print("It is located in " + self.location.title() + ".")
         print(self.cuisine.title() + " cuisine is used.")
-
+        print(self.name.title() + " has served " + str(self.number_served) + ' customers.')
+        
     def open(self):
         """Simulate the time a restaurant opens."""
         print(self.name.title() + " is always open.")
 
+    def set_number_served(self, number):
+        """Sets the number of customers that have been served."""
+        if number >= self.number_served:
+            self.number_served = number
+        else:
+            print("You can't have a negative number.")
+      
+    def increment_number_served(self, number):
+        """Increase the number of customers served."""
+        self.number_served += number
+    
 
 restaurant_1 = Restaurant('greenland', 'chineese', 'swedru', 4)
 restaurant_2 = Restaurant('golden tulip', 'spanish', 'accra', 5)
@@ -25,14 +38,19 @@ restaurant_3 = Restaurant('movempick', 'african', 'accra', 5)
 
 
 print(restaurant_1.name.title() + " is my favorite restaurant.")
+restaurant_1.set_number_served(34)
 restaurant_1.describe_restaurant()
 restaurant_1.open()
 
-print("\n " + restaurant_2.name.title() + ' is one of my favorite.')
+print("\n" + restaurant_2.name.title() + ' is one of my favorite.')
+restaurant_2.set_number_served(869)
+restaurant_2.increment_number_served(100)
 restaurant_2.describe_restaurant()
 restaurant_2.open()
 
-print("\n " + restaurant_3.name.title() + ' is another one of my favorite restaurants.')
+print("\n" + restaurant_3.name.title() + ' is another one of my favorite restaurants.')
+restaurant_3.set_number_served(57)
+restaurant_3.increment_number_served(9)
 restaurant_3.describe_restaurant()
 restaurant_3.open()
 
--- classes/users.py
+++ classes/users.py
@@ -9,21 +9,41 @@ class User():
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+        self.login_attempts = 0
 
     def describe_user(self):
         """Describe the user."""
         print(self.first_name.title() + " " + self.last_name.title() + " ist " + str(self.age) + " Jahre.")
+        print(self.first_name.title() + ' hat ' + str(self.login_attempts) + ' mal versucht sich anzumelden.')
 
     def greet_user(self):
         """Greet the user."""
         full_name = self.first_name + " " + self.last_name
         print("\nHallo " + full_name.title() + " gerne geschehen.")
+    
+    def increment_login_attempts(self):
+        """Increment the value of login attempts by 1."""
+        self.login_attempts += 1
 
+    def reset_login_attempts(self):
+        """Reset login attempts to Zero 0."""
+        self.login_attempts = 0
+
+    def __repr__(self):
+        return (f'{self.__class__.__name__}('
+                f'{self.first_name!r}, {self.last_name!r}, {self.age!r})')
+                
 
 user = User('ebo', 'asibu', 29)
 print("\n " + user.first_name.title() + " ist " + str(user.age) + " Jahre.")
 user.greet_user()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
 user.describe_user()
+user.reset_login_attempts()
+user.describe_user()
+
 
 user_2 = User('ato', 'asibu', 18)
 print("\n " + user_2.first_name.title() + " ist " + str(user_2.age) + " Jahre.")
