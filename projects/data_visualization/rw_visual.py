633efc748bd0ceda2196deebe7b68502ea5f8810
Bernard Asibu <44650300+asibubernard@users.noreply.github.com>
2022-12-01 06:23:42 +0000

    modified files in the projects folder

--- projects/data_visualization/rw_visual.py
+++ projects/data_visualization/rw_visual.py
@@ -5,20 +5,18 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk, and plot the points.
-    rw = RandomWalk(20000)
+    rw = RandomWalk(5000)
     rw.fill_walk()
    
     # set the size of the plotting window
     plt.figure(dpi=128, figsize=(10, 6))
 
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-                edgecolor='none', s=15)
+    plt.plot(rw.x_values, rw.y_values, linewidth=5)
     
     # Emphasize the first and last points.
-    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
-                s=100)
+    plt.plot(0, 0, c='green')
+    plt.plot(rw.x_values[-1], rw.y_values[-1])
 
     # Remove the axes.
     plt.axis('off')
@@ -28,7 +26,5 @@ while True:
     keep_running = input("Make another walk? (y/n): ")
     if keep_running == 'n':
         break
-
-
 
 
